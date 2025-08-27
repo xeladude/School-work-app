4a8460b7d268c827d86a565dba4eb981545de9ff
xeladude <slimmygames2021@gmail.com>
2025-08-26 18:05:09 -0600

    made everything work + remove button

--- main.js
+++ main.js
@@ -23,6 +23,8 @@ document.getElementById("task-form").addEventListener("submit", function (e){
 
 });
 
+
+
 function renderTasks(){
     const tableBody = document.getElementById("tasks-body");
 
@@ -32,23 +34,51 @@ function renderTasks(){
         .forEach(task =>{
             const row = document.createElement("tr");
             
-            const titleCell = document.createElement("th");
+            const titleCell = document.createElement("td");
             titleCell.textContent=task.title;
             if (task.completed)titleCell.classList.add("task-completed")
            
-            const subjectCell = document.createElement("th");
+            const subjectCell = document.createElement("td");
             subjectCell.textContent=task.subject;
             if (task.completed)subjectCell.classList.add("task-completed")
             
-            const dueDateCell = document.createElement("th");
+            const dueDateCell = document.createElement("td");
             dueDateCell.textContent=task.dueDate;
             if (task.completed)dueDateCell.classList.add("task-completed")
 
-        })
-};
+            const completedCell = document.createElement("td");
+            const checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
+            checkbox.checked = task.completed;
+
+            checkbox.addEventListener("change", () => {
+                task.completed = checkbox.checked;
+                renderTasks();
+            });
+            completedCell.appendChild(checkbox);
+            if (task.completed){
+                const removeButton = document.createElement("button");
+                removeButton.textContent = "Remove";
+                removeButton.className = "remove-button";
+                removeButton.addEventListener("click",() => {
+                    tasks = tasks.filter(t => t.id !== task.id)
+                    renderTasks()
+                });
+                completedCell.appendChild(removeButton);
+            }
+            
+            row.appendChild(titleCell);
+            row.appendChild(subjectCell);
+            row.appendChild(dueDateCell);
+            row.appendChild(completedCell);
+
+            tableBody.appendChild(row);
+
+        });  
+}
 
 
-
+renderTasks();
 
 
 
